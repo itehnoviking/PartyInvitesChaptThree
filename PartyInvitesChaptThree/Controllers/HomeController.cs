a2f27bae5f9b2fa8181c440b007fac544b7c306f
Иван К. PC <itehnoviking@gmail.com>
2022-01-09 02:01:44 +0300

    Add project files.

---
+++ PartyInvitesChaptThree/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvitesChaptThree.Models;
+using System.Diagnostics;
+
+namespace PartyInvitesChaptThree.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
